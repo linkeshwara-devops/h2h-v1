0c362dff11f7551b7c2602acf25478c54b5d1713
Linkeshwara N S <linkeshwara.off@gmail.com>
2026-06-18 08:01:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "ap-south-1a"
+}
 
+resource "ami-00427e38fe4ed73c1" {
+  ami           = "REPLACE_WITH_YOUR_AMI"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-server"
+  }
+}
